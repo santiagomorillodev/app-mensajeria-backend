45271b0e7ab9e2d66dd9a999e8ef8352144672c4
santiagomorillodev <santiagomorillodev@gmail.com>
2026-08-18 20:53:48 -0400

    agregando dependencia

--- models/message_model.py
+++ models/message_model.py
@@ -1,16 +1,15 @@
 from config import Base
-from sqlalchemy import Column, Text, Integer, ForeignKey, func, DateTime, Boolean, String
+from sqlalchemy import Column, Text, Integer, ForeignKey, func, DateTime, Boolean
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 from datetime import datetime
 
 class MessageModel(Base):
     __tablename__ = 'messages'
+    
     message_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     sender_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
     conversation_id: Mapped[int] = mapped_column(Integer, ForeignKey('conversations.id', ondelete='CASCADE'), nullable=True)
-    content: Mapped[str] = mapped_column(Text, nullable=True)
-    image_url: Mapped[str] = mapped_column(Text, nullable=True)
-    public_id: Mapped[int] = mapped_column(String(500), nullable=True)
+    content: Mapped[str] = mapped_column(Text, nullable=False)  # 👈 Cambiado a nullable=False si solo habrá texto
     created: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     status: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
 
